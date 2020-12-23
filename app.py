1ee061331d473c815697d3841659540cb8323ba1
raphaelmar <raphaelmar@hotmail.fr>
2020-12-23 10:12:17 +0000

    added user's profile

--- app.py
+++ app.py
@@ -47,7 +47,44 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("email").lower()
         flash("Registration Successful")
+        return redirect(url_for("profile", email=session["user"]))
+
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        #check if email already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"email": request.form.get("email").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("email").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("email")))
+                return redirect(url_for(
+                    "profile", email=session["user"]))
+            else:
+                #invalid password match
+                flash("Incorrect email and/or password")
+                return redirect (url_for("login"))
+
+        else:
+            # email doesn't exist
+            flash("Incorrect email and/or password")
+    return render_template("login.html")
+
+
+@app.route("/profile/<email>", methods=["GET", "POST"])
+def profile(email):
+    # grab the sessions user's email form db
+    email = mongo.db.users.find_one(
+        {"email": session["user"]})["email"]
+    return render_template("profile.html", email=email)
 
 
 if __name__ == "__main__":
