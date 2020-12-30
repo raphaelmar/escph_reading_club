892842ba2b0696e2cd7ee6fd718f4680ee6d9e61
raphaelmar <raphaelmar@hotmail.fr>
2020-12-30 14:37:05 +0000

    debug mode off

--- app.py
+++ app.py
@@ -173,4 +173,4 @@ def like(review_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
